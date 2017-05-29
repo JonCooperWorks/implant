140e90e91eff2324391566ba7b3f4274ed2167a1
Jonathan Cooper <cooper@cooperthecoder.com>
2017-05-29 01:20:35 -0500

    Removed caching of current Stage in redressing attack in favour of caching the current index

--- app/src/main/java/com/cooperthecoder/implant/RedressingAttack.kt
+++ app/src/main/java/com/cooperthecoder/implant/RedressingAttack.kt
@@ -3,20 +3,18 @@ package com.cooperthecoder.implant
 
 class RedressingAttack(val stages: List<Stage>) {
 
-    private var currentStage: Stage? = null
     private var attackIndex = -1
 
     private fun moveToStage(index: Int) {
         val stage = stages.getOrNull(index)
         if (stage != null) {
             stage.drawOnScreen()
-            currentStage = stage
             attackIndex = index
         }
     }
 
     fun moveToNext(): Boolean {
-        currentStage?.clearFromScreen()
+        stages.getOrNull(attackIndex)?.clearFromScreen()
         val next = attackIndex + 1
         if (next < stages.size) {
             moveToStage(next)
@@ -27,6 +25,6 @@ class RedressingAttack(val stages: List<Stage>) {
     }
 
     fun stopAttack() {
-        currentStage?.clearFromScreen()
+        stages.getOrNull(attackIndex)?.clearFromScreen()
     }
 }
